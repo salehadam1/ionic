8b34276b2094ec0aae4b5b859d474612833df87b
SALEH ADAM - STUDENT <G00374853@gmit.ie>
2020-03-09 18:02:49 +0000

    first commit

---
+++ src/app/leinster-counties/leinster-counties.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leinster-counties',
+  templateUrl: './leinster-counties.page.html',
+  styleUrls: ['./leinster-counties.page.scss'],
+})
+export class LeinsterCountiesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
